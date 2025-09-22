da39a4ef6ea4e6dc8c5ec70b4a66bf58bf8fe54a
Steve Atkinson <steve.atkinson@windswept.org.uk>
2025-09-22 21:05:16 +0100

    Add GitHub Actions workflow for automated deployment to GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/react-three-house/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets'
+  }
 })
