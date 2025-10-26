f0d08aacf2ed6ee8c5ca9b047a22960d634ba4be
Steve Atkinson <steve.atkinson@windswept.org.uk>
2025-10-26 17:04:30 +0000

    Resize kitchen walls

--- src/components/kitchen/Kitchen.jsx
+++ src/components/kitchen/Kitchen.jsx
@@ -3,28 +3,28 @@ import Wall from '../Wall'
 
 export default function Kitchen({
     position = [0, 0, 0],
-    floorColor = "blue",
-    wallColor = "red" }) {
+    floorColor = "#8b7355",
+    wallColor = "#ffffff" }) {
     return (
         <group position={position}>
             {/* Kitchen floor */}
             <Floor
-                args={[5.630, 0.2, 4.200]}
-                position={[0, -0.1, 0]}
+                args={[5.630, 0.1, 4.200]}
+                position={[0, -0.05, 0]}
                 color={floorColor}
             />
 
             {/* Back wall */}
             <Wall
-                args={[5.630, 3, 0.2]}
-                position={[0, 1.5, 2.2]}
+                args={[5.630, 3, 0.1]}
+                position={[0, 1.5, 2.15]}
                 color={wallColor}
             />
 
             {/* Side wall */}
             <Wall
-                args={[0.2, 3, 4.2]}
-                position={[-(5.630 / 2) - 0.1, 1.5, 0]}
+                args={[0.1, 3, 4.2]}
+                position={[-(5.630 / 2) - 0.05, 1.5, 0]}
                 color={wallColor}
             />
         </group>
